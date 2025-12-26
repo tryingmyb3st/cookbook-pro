2206ee28e90e125197c96f4b73c807ad9552da00
Maxim Khoroshkin <khoroskinmaks@gmail.com>
2025-12-26 19:44:34 +0300

    fix: api_url

--- src/Cookbook/CookbookClient/cookbook-client/src/services/api.js
+++ src/Cookbook/CookbookClient/cookbook-client/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://backend:5000';
+const API_BASE_URL = 'http://localhost:5001';
 
 export const api = axios.create({
   baseURL: API_BASE_URL,
